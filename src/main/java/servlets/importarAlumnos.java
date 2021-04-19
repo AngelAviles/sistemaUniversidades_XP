f23d071b007ea5e435a8a128bb583c514889e58d
Angel Armando Aviles Martinez <avilesm.angel@gmail.com>
2021-04-18 22:47:34 -0700

    Importar alumnos acepta archivos .xls y .csv, la etiqueta de ayuda despliega un mensaje y da una plantilla en Excel, y la tabla se puede ordenar.

--- src/main/java/servlets/importarAlumnos.java
+++ src/main/java/servlets/importarAlumnos.java
@@ -5,16 +5,23 @@
  */
 package servlets;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 import dao.AlumnoJpaController;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.servlet.ServletException;
@@ -30,6 +37,7 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -41,16 +49,6 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 @MultipartConfig
 public class importarAlumnos extends HttpServlet {
 
-//    private boolean isMultipart;
-//    private String filePath;
-//    private int maxFileSize = 50 * 1024;
-//    private int maxMemSize = 4 * 1024;
-//    private File file;
-//
-//    @Override
-//    public void init() throws ServletException {
-//        filePath = getServletContext().getInitParameter("file-upload");
-//    }
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -92,13 +90,55 @@ public class importarAlumnos extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         HttpSession session = request.getSession();
 
         Part filePart = request.getPart("archivo"); // Retrieves <input type="file" name="archivo">
         String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
         InputStream fileContent = filePart.getInputStream();
 
+        List alumnos = new ArrayList<Alumno>();
+
+        switch (getFileExtension(fileName)) {
+            case "xlsx":
+                alumnos = importarXLSX(fileContent);
+                break;
+            case "xls":
+                alumnos = importarXLS(fileContent);
+                break;
+            case "csv":
+            {
+                try {
+                    alumnos = importarCSV(fileContent);
+                } catch (CsvValidationException ex) {
+                    Logger.getLogger(importarAlumnos.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+                break;
+
+        }
+
+        session.setAttribute("listaAlumnos", alumnos);
+        response.sendRedirect("importarAlumnos.jsp");
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    public static String getFileExtension(String name) {
+        int dotIndex = name.lastIndexOf('.');
+        return (dotIndex == -1) ? "" : name.substring(dotIndex + 1);
+    }
+
+    public static List<Alumno> importarXLSX(InputStream fileContent) throws IOException {
         XSSFWorkbook wb = new XSSFWorkbook(fileContent);
         XSSFSheet sheet = wb.getSheetAt(0);
 
@@ -123,88 +163,62 @@ public class importarAlumnos extends HttpServlet {
             alumnos.add(alumno);
         }
 
-        session.setAttribute("listaAlumnos", alumnos);
-        response.sendRedirect("importarAlumnos.jsp");
-
-//        // Check that we have a file upload request
-//        isMultipart = ServletFileUpload.isMultipartContent(request);
-//        response.setContentType("text/html");
-//        java.io.PrintWriter out = response.getWriter();
-//
-//        if (!isMultipart) {
-//            out.println("<html>");
-//            out.println("<head>");
-//            out.println("<title>Servlet upload</title>");
-//            out.println("</head>");
-//            out.println("<body>");
-//            out.println("<p>No file uploaded</p>");
-//            out.println("</body>");
-//            out.println("</html>");
-//            return;
-//        }
-//
-//        DiskFileItemFactory factory = new DiskFileItemFactory();
-//
-//        // maximum size that will be stored in memory
-//        factory.setSizeThreshold(maxMemSize);
-//
-//        // Location to save data that is larger than maxMemSize.
-//        factory.setRepository(new File("c:\\temp"));
-//
-//        // Create a new file upload handler
-//        ServletFileUpload upload = new ServletFileUpload(factory);
-//
-//        // maximum file size to be uploaded.
-//        upload.setSizeMax(maxFileSize);
-//
-//        try {
-//            // Parse the request to get file items.
-//            List fileItems = upload.parseRequest(request);
-//
-//            // Process the uploaded file items
-//            Iterator i = fileItems.iterator();
-//
-//            out.println("<html>");
-//            out.println("<head>");
-//            out.println("<title>Servlet upload</title>");
-//            out.println("</head>");
-//            out.println("<body>");
-//
-//            while (i.hasNext()) {
-//                FileItem fi = (FileItem) i.next();
-//                if (!fi.isFormField()) {
-//                    // Get the uploaded file parameters
-//                    String fieldName = fi.getFieldName();
-//                    String fileName = fi.getName();
-//                    String contentType = fi.getContentType();
-//                    boolean isInMemory = fi.isInMemory();
-//                    long sizeInBytes = fi.getSize();
-//
-//                    // Write the file
-//                    if (fileName.lastIndexOf("\\") >= 0) {
-//                        file = new File(filePath + fileName.substring(fileName.lastIndexOf("\\")));
-//                    } else {
-//                        file = new File(filePath + fileName.substring(fileName.lastIndexOf("\\") + 1));
-//                    }
-//                    fi.write(file);
-//                    out.println("Uploaded Filename: " + fileName + "<br>");
-//                }
-//            }
-//            out.println("</body>");
-//            out.println("</html>");
-//        } catch (Exception ex) {
-//            System.out.println(ex);
-//        }
+        return alumnos;
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+    public static List<Alumno> importarXLS(InputStream fileContent) throws IOException {
+        Workbook wb = new HSSFWorkbook(fileContent);
+        Sheet sheet = wb.getSheetAt(0);
+
+        int numFilas = sheet.getLastRowNum();
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("sistemaUniversidades_XP_PU");
+        AlumnoJpaController jpa = new AlumnoJpaController(factory);
+
+        List alumnos = new ArrayList<Alumno>();
+
+        for (int a = 0; a <= numFilas; a++) {
+            Row fila = sheet.getRow(a);
+
+            Alumno alumno = new Alumno(null,
+                    fila.getCell(0).getStringCellValue(),
+                    fila.getCell(1).getStringCellValue(),
+                    fila.getCell(2).getStringCellValue(),
+                    null);
+
+            jpa.create(alumno);
+
+            alumnos.add(alumno);
+        }
+
+        return alumnos;
+    }
+
+    public static List<Alumno> importarCSV(InputStream fileContent) throws IOException, CsvValidationException {
+        CSVReader csv = new CSVReader(new InputStreamReader(fileContent));
+
+        String[] fila = null;
+        
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("sistemaUniversidades_XP_PU");
+        AlumnoJpaController jpa = new AlumnoJpaController(factory);
+
+        List alumnos = new ArrayList<Alumno>();
+        
+        while ((fila = csv.readNext()) != null) {
+            Alumno alumno = new Alumno(null,
+                    fila[0],
+                    fila[1],
+                    fila[2],
+                    null);
+
+            jpa.create(alumno);
+
+            alumnos.add(alumno);
+        }
+
+        csv.close();
+
+        return alumnos;
+    }
 
 }
--- src/main/webapp/importarAlumnos.jsp
+++ src/main/webapp/importarAlumnos.jsp
@@ -1,17 +1,11 @@
-<%-- 
-    Document   : importarAlumnos
-    Created on : 12/04/2021, 09:46:32 PM
-    Author     : angel
---%>
+<%-- Document : importarAlumnos Created on : 12/04/2021, 09:46:32 PM Author : angel --%>
 
-<%@page import="objetosNegocio.Alumno"%>
-<%@page import="java.util.List"%>
-<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@page import="objetosNegocio.Alumno" %>
+<%@page import="java.util.List" %>
+<%@page contentType="text/html" pageEncoding="UTF-8" %>
 
-<%
-    List lista = (List) session.getAttribute("listaAlumnos");
-    session.removeAttribute("listaAlumnos");
-%>
+<% List lista = (List) session.getAttribute("listaAlumnos");
+    session.removeAttribute("listaAlumnos"); %>
 
 <!DOCTYPE html>
 <html>
@@ -20,6 +14,8 @@
         <title>Importar Alumnos</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
               integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.24/datatables.min.css"/>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     </head>
 
@@ -49,14 +45,20 @@
             </nav>
             <hr />
 
-            <div class="container-fluid ">
+            <div class="container-fluid">
                 <div class="row align-items-center justify-content-center">
                     <div class="col-auto">
                         <div class="border border-black border-3 mb-3">
                             <form action="importarAlumnos" method="post" enctype="multipart/form-data">
                                 <div class="m-3">
-                                    <label for="formFileSm" class="form-label">Archivo Excel o CSV</label>
-                                    <input class="form-control form-control-sm" id="formFileSm" name="archivo" type="file" <%--accept=".xls, .xlsx, .csv"--%> >
+                                    <label for="formFileSm" class="form-label">Archivo Excel o
+                                        CSV</label>
+                                    <div class="row m-auto">
+                                        <input class="form-control form-control-sm col" id="formFileSm" name="archivo"
+                                               type="file" accept=".xls, .xlsx, .csv" required/>
+                                        <i class="fa fa-question-circle col-sm-auto" title="Formato del archivo"
+                                           data-bs-content="El archivo debe contener la información de los alumnos ordenados de la siguiente manera: matricula, nombre y CURP. Puedes utilizar esta <a href='recursos/plantilla_importar_alumnos.xlsx' download='Plantilla de Importar Alumnos.xlsx'>plantilla</a>." data-bs-toggle="popover" data-bs-html="true" ></i>
+                                    </div>
                                 </div>
                                 <div class="m-3">
                                     <div class="d-grid gap-6 mx-auto">
@@ -77,25 +79,24 @@
                         </div>
                     </div>
                     <div class="table-responsive col-md">
-                        <table class="table table-striped border border-black border-3" id="alumnosTabla">
+                        <table id="alumnosTabla" class="table table-striped border border-black border-3">
                             <thead class="table-dark">
                                 <tr>
-                                    <th scope="col">ID</th>
+                                    <th scope="col">Matricula</th>
                                     <th scope="col">Nombre</th>
                                     <th scope="col">CURP</th>
                                 </tr>
                             </thead>
                             <tbody>
 
-                                <%
-                                    if (lista != null) {
+                                <% if (lista != null) {
                                         for (int i = 0; i < lista.size(); i++) {
                                             Alumno a = (Alumno) lista.get(i);
                                             out.print("<tr>"
                                                     + "<td scope=\"row\">" + a.getMatricula() + "</td>"
                                                     + "<td>" + a.getNombre() + "</td>"
                                                     + "<td>" + a.getCurp() + "</td>"
-                                                    + "<tr>"
+                                                    + "</tr>"
                                             );
                                         }
                                     } else {
@@ -103,8 +104,7 @@
                                                 + "<td scope=\"row\">1</td>"
                                                 + "<td>Angel</td>"
                                                 + "<td>curp</td>"
-                                                + "<tr>"
-                                        );
+                                                + "</tr>");
                                     }
 
 
@@ -116,5 +116,15 @@
                 </div>
             </div>
         </article>
+
+
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
+        crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.24/datatables.min.js"></script>
+        <script src="js/app.js" charset="utf-8"></script>
     </body>
+
 </html>
